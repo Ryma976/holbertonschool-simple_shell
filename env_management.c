91e2e1958a6f227e2109604c0e511b713f1a381e
reem <ryma6110@gmail.com>
2026-05-14 15:34:59 +0000

    Task 13: Final clean logic for environment

--- env_management.c
+++ env_management.c
@@ -10,7 +10,6 @@ int _env(void)
 
 	if (environ == NULL)
 		return (0);
-
 	for (i = 0; environ[i]; i++)
 	{
 		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
@@ -21,22 +20,27 @@ int _env(void)
 
 /**
  * _setenv - Initialize or modify an environment variable
- * @argv: array of arguments
+ * @argv: array of arguments (argv[1]=NAME, argv[2]=VALUE)
  * Return: 0 on success, -1 on failure
  */
 int _setenv(char **argv)
 {
 	char *new_var;
-	int i, len;
+	char **new_environ;
+	int i, len, count;
 
 	if (!argv[1] || !argv[2])
+	{
+		write(STDERR_FILENO, "setenv: Usage: setenv VARIABLE VALUE\n", 37);
 		return (-1);
-
+	}
 	len = strlen(argv[1]) + strlen(argv[2]) + 2;
 	new_var = malloc(len);
 	if (!new_var)
+	{
+		write(STDERR_FILENO, "setenv: malloc failed\n", 22);
 		return (-1);
-
+	}
 	strcpy(new_var, argv[1]);
 	strcat(new_var, "=");
 	strcat(new_var, argv[2]);
@@ -46,25 +50,30 @@ int _setenv(char **argv)
 		if (strncmp(environ[i], argv[1], strlen(argv[1])) == 0 &&
 		    environ[i][strlen(argv[1])] == '=')
 		{
-			/* 
-			 * We overwrite the pointer.
-			 * IMPORTANT: In this specific project, the checker 
-			 * often fails if you try to free() original environ 
-			 * pointers because they aren't on the heap.
-			 */
 			environ[i] = new_var;
 			return (0);
 		}
 	}
-	
-	environ[i] = new_var;
-	environ[i + 1] = NULL;
+
+	count = i;
+	new_environ = malloc((count + 2) * sizeof(char *));
+	if (!new_environ)
+	{
+		free(new_var);
+		write(STDERR_FILENO, "setenv: malloc failed\n", 22);
+		return (-1);
+	}
+	for (i = 0; i < count; i++)
+		new_environ[i] = environ[i];
+	new_environ[count] = new_var;
+	new_environ[count + 1] = NULL;
+	environ = new_environ;
 	return (0);
 }
 
 /**
  * _unsetenv - Remove an environment variable
- * @argv: array of arguments
+ * @argv: array of arguments (argv[1]=NAME)
  * Return: 0 on success, -1 on failure
  */
 int _unsetenv(char **argv)
@@ -72,8 +81,10 @@ int _unsetenv(char **argv)
 	int i, j, len;
 
 	if (!argv[1])
+	{
+		write(STDERR_FILENO, "unsetenv: Usage: unsetenv VARIABLE\n", 35);
 		return (-1);
-
+	}
 	len = strlen(argv[1]);
 	for (i = 0; environ[i]; i++)
 	{
@@ -84,5 +95,6 @@ int _unsetenv(char **argv)
 			return (0);
 		}
 	}
-	return (0);
+	write(STDERR_FILENO, "unsetenv: variable not found\n", 29);
+	return (-1);
 }
