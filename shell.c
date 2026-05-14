2bd407cb1a40270933426c93b904cb60c806f221
reem <ryma6110@gmail.com>
2026-05-14 14:50:44 +0000

    Fix: Add sigint_handler to shell.h and integrate environment built-ins

--- shell.c
+++ shell.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * main - entry point for the simple shell
+ * main - Entry point for the shell
  * Return: last exit status
  */
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 	char **argv;
 	int i, last_status = 0;
 
-	/* Register the Ctrl+C signal handler */
+	/* Register Ctrl+C handler */
 	signal(SIGINT, sigint_handler);
 
 	while (1)
@@ -27,7 +27,6 @@ int main(void)
 				write(STDOUT_FILENO, "\n", 1);
 			break;
 		}
-
 		if (line[read_status - 1] == '\n')
 			line[read_status - 1] = '\0';
 
@@ -38,14 +37,44 @@ int main(void)
 			continue;
 		}
 
+		/* Built-ins check */
 		if (strcmp(argv[0], "exit") == 0)
 			handle_exit(argv, line, last_status);
-
-		execute_command(argv, line, &last_status);
+		else if (strcmp(argv[0], "setenv") == 0)
+			_setenv(argv);
+		else if (strcmp(argv[0], "unsetenv") == 0)
+			_unsetenv(argv);
+		else
+			execute_command(argv, line, &last_status);
 
 		for (i = 0; argv[i]; i++) free(argv[i]);
 		free(argv);
 	}
 	free(line);
 	return (last_status);
+}
+
+void execute_command(char **argv, char *line, int *last_status)
+{
+	pid_t child_pid;
+	int status, i;
+
+	child_pid = fork();
+	if (child_pid == 0)
+	{
+		if (execve(argv[0], argv, environ) == -1)
+		{
+			perror("./hsh");
+			for (i = 0; argv[i]; i++) free(argv[i]);
+			free(argv);
+			free(line);
+			_exit(127);
+		}
+	}
+	else
+	{
+		wait(&status);
+		if (WIFEXITED(status))
+			*last_status = WEXITSTATUS(status);
+	}
 }
--- shell.h
+++ shell.h
@@ -7,16 +7,24 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <signal.h>
 
 #define READ_BUF_SIZE 1024
 
 extern char **environ;
 
-/* Prototypes updated to include last_status */
+/* Prototypes */
 ssize_t _getline(char **lineptr, size_t *n, int fd);
 char **strtow(char *str, char *d);
 void handle_exit(char **argv, char *line, int last_status);
 void execute_command(char **argv, char *line, int *last_status);
 int _erratoi(char *s);
+
+/* Signal Handling (Task 12) */
+void sigint_handler(int sig);
+
+/* Environment Management (Task 13) */
+int _setenv(char **argv);
+int _unsetenv(char **argv);
 
 #endif
