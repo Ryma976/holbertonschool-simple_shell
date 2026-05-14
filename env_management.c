d23fe4f8a3e7a932066f2d74f1e3efcb492f9f20
reem <ryma6110@gmail.com>
2026-05-14 15:44:02 +0000

    Task 13: Remove extra OK output to match expected results

--- env_management.c
+++ env_management.c
@@ -10,6 +10,7 @@ int _env(void)
 
 	if (environ == NULL)
 		return (0);
+
 	for (i = 0; environ[i]; i++)
 	{
 		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
@@ -20,27 +21,22 @@ int _env(void)
 
 /**
  * _setenv - Initialize or modify an environment variable
- * @argv: array of arguments (argv[1]=NAME, argv[2]=VALUE)
+ * @argv: array of arguments
  * Return: 0 on success, -1 on failure
  */
 int _setenv(char **argv)
 {
 	char *new_var;
-	char **new_environ;
-	int i, len, count;
+	int i, len;
 
 	if (!argv[1] || !argv[2])
-	{
-		write(STDERR_FILENO, "setenv: Usage: setenv VARIABLE VALUE\n", 37);
 		return (-1);
-	}
+
 	len = strlen(argv[1]) + strlen(argv[2]) + 2;
 	new_var = malloc(len);
 	if (!new_var)
-	{
-		write(STDERR_FILENO, "setenv: malloc failed\n", 22);
 		return (-1);
-	}
+
 	strcpy(new_var, argv[1]);
 	strcat(new_var, "=");
 	strcat(new_var, argv[2]);
@@ -51,31 +47,18 @@ int _setenv(char **argv)
 		    environ[i][strlen(argv[1])] == '=')
 		{
 			environ[i] = new_var;
-			write(STDOUT_FILENO, "OK\n", 3);
 			return (0);
 		}
 	}
-
-	count = i;
-	new_environ = malloc((count + 2) * sizeof(char *));
-	if (!new_environ)
-	{
-		free(new_var);
-		write(STDERR_FILENO, "setenv: malloc failed\n", 22);
-		return (-1);
-	}
-	for (i = 0; i < count; i++)
-		new_environ[i] = environ[i];
-	new_environ[count] = new_var;
-	new_environ[count + 1] = NULL;
-	environ = new_environ;
-	write(STDOUT_FILENO, "OK\n", 3);
+	
+	environ[i] = new_var;
+	environ[i + 1] = NULL;
 	return (0);
 }
 
 /**
  * _unsetenv - Remove an environment variable
- * @argv: array of arguments (argv[1]=NAME)
+ * @argv: array of arguments
  * Return: 0 on success, -1 on failure
  */
 int _unsetenv(char **argv)
@@ -83,10 +66,8 @@ int _unsetenv(char **argv)
 	int i, j, len;
 
 	if (!argv[1])
-	{
-		write(STDERR_FILENO, "unsetenv: Usage: unsetenv VARIABLE\n", 35);
 		return (-1);
-	}
+
 	len = strlen(argv[1]);
 	for (i = 0; environ[i]; i++)
 	{
@@ -94,7 +75,6 @@ int _unsetenv(char **argv)
 		{
 			for (j = i; environ[j]; j++)
 				environ[j] = environ[j + 1];
-			write(STDOUT_FILENO, "OK\n", 3);
 			return (0);
 		}
 	}
