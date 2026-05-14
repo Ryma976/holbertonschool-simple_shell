dc53d7c06635b8fb932aacc59560d361bb5a1c84
reem <ryma6110@gmail.com>
2026-05-14 14:19:43 +0000

    Fix: Remove duplicate _getline definition from shell.c

--- shell.c
+++ shell.c
@@ -1,140 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
+#include "shell.h"
 
-#define READ_BUF_SIZE 1024
-
-extern char **environ;
-
-ssize_t _getline(char **lineptr, size_t *n, int fd)
-{
-	static char buf[READ_BUF_SIZE];
-	static size_t i, len;
-	size_t count = 0;
-	char c;
-
-	if (*lineptr == NULL || *n == 0)
-	{
-		*n = READ_BUF_SIZE;
-		*lineptr = malloc(*n);
-		if (!(*lineptr))
-			return (-1);
-	}
-
-	while (1)
-	{
-		if (i >= len)
-		{
-			len = read(fd, buf, READ_BUF_SIZE);
-			i = 0;
-			if (len <= 0)
-				return (count == 0 ? -1 : (ssize_t)count);
-		}
-		c = buf[i++];
-		if (count >= *n - 1)
-		{
-			*n += READ_BUF_SIZE;
-			*lineptr = realloc(*lineptr, *n);
-			if (!(*lineptr))
-				return (-1);
-		}
-		(*lineptr)[count++] = c;
-		if (c == '\n')
-			break;
-	}
-	(*lineptr)[count] = '\0';
-	return (count);
-}
-
-char *find_path(char *command)
-{
-	char *path = NULL, *path_copy, *token, *full_path;
-	struct stat st;
-	int i = 0;
-
-	if (strchr(command, '/') != NULL)
-		return (access(command, X_OK) == 0 ? strdup(command) : NULL);
-
-	while (environ[i])
-	{
-		if (strncmp(environ[i], "PATH=", 5) == 0)
-		{
-			path = environ[i] + 5;
-			break;
-		}
-		i++;
-	}
-	if (!path)
-		return (NULL);
-
-	path_copy = strdup(path);
-	token = strtok(path_copy, ":");
-	while (token)
-	{
-		full_path = malloc(strlen(token) + strlen(command) + 2);
-		if (!full_path) return (NULL);
-		sprintf(full_path, "%s/%s", token, command);
-		if (stat(full_path, &st) == 0)
-		{
-			free(path_copy);
-			return (full_path);
-		}
-		free(full_path);
-		token = strtok(NULL, ":");
-	}
-	free(path_copy);
-	return (NULL);
-}
-
+/**
+ * main - simple shell with custom _getline
+ * Return: 0 on success
+ */
 int main(void)
 {
-	char *line = NULL, *argv[64], *path;
-	size_t n = 0;
-	ssize_t nread;
-	int i, status;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read_status;
+	char *argv[100];
+	int i;
 	pid_t child_pid;
+	int status;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
-
-		nread = _getline(&line, &n, STDIN_FILENO);
-		if (nread == -1)
+			write(STDOUT_FILENO, "($) ", 4);read_status = _getline(&line, &len, STDIN_FILENO);
+		
+		if (read_status == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
 			break;
+		}
 
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
+		if (line[read_status - 1] == '\n')
+			line[read_status - 1] = '\0';
 
 		i = 0;
-		argv[i] = strtok(line, " \t\n");
-		if (!argv[i])
+		argv[i] = strtok(line, " \t\r\n\a");
+		while (argv[i] != NULL)
+		{
+			i++;
+			argv[i] = strtok(NULL, " \t\r\n\a");
+		}
+
+		if (argv[0] == NULL)
 			continue;
 
-		if (strcmp(argv[0], "exit") == 0)
-			break;
-
-		path = find_path(argv[0]);
-		if (path)
+		child_pid = fork();
+		if (child_pid == 0)
 		{
-			child_pid = fork();
-			if (child_pid == 0)
-			{
-				while (argv[i])
-					argv[++i] = strtok(NULL, " \t\n");
-				execve(path, argv, environ);
-				perror("Error");
-				exit(1);
-			}
-			wait(&status);
-			free(path);
+			if (execve(argv[0], argv, environ) == -1)
+				perror("./hsh");
+			exit(EXIT_FAILURE);
 		}
 		else
-			perror("./hsh");
+		{
+			wait(&status);
+		}
 	}
 	free(line);
 	return (0);
