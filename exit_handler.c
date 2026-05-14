980db5f26f315fbf52eec0405c09ecabd58d5635
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-05-14 17:03:46 +0000

    Fix task 13 exit_handler.c

--- exit_handler.c
+++ exit_handler.c
@@ -1,34 +1,45 @@
 #include "shell.h"
 
 /**
- * _erratoi - string to int
- * @s: string
- * Return: int or -1
+ * _erratoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: integer value, or -1 on error
  */
 int _erratoi(char *s)
 {
 	int i = 0;
 	unsigned long int result = 0;
 
-	if (*s == '+') s++;
+	if (*s == '+')
+		s++;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result *= 10;
 			result += (s[i] - '0');
-			if (result > 2147483647) return (-1);
+
+			if (result > 2147483647)
+				return (-1);
 		}
-		else return (-1);
+		else
+		{
+			return (-1);
+		}
 	}
+
 	return ((int)result);
 }
 
 /**
- * handle_exit - handles exit logic
- * @argv: args
- * @line: buffer
- * @last_status: status
+ * handle_exit - handles the exit command
+ * @argv: command arguments
+ * @line: input line
+ * @last_status: last command status
+ *
+ * Return: nothing
  */
 void handle_exit(char **argv, char *line, int last_status)
 {
@@ -37,23 +48,36 @@ void handle_exit(char **argv, char *line, int last_status)
 	if (argv[1])
 	{
 		exit_val = _erratoi(argv[1]);
+
 		if (exit_val == -1)
 		{
 			write(STDERR_FILENO, "./hsh: 1: exit: Illegal number: ", 32);
 			write(STDERR_FILENO, argv[1], strlen(argv[1]));
 			write(STDERR_FILENO, "\n", 1);
-			for (i = 0; argv[i]; i++) free(argv[i]);
+
+			for (i = 0; argv[i]; i++)
+				free(argv[i]);
+
 			free(argv);
 			free(line);
+			_free_env();
 			exit(2);
 		}
-		for (i = 0; argv[i]; i++) free(argv[i]);
+
+		for (i = 0; argv[i]; i++)
+			free(argv[i]);
+
 		free(argv);
 		free(line);
+		_free_env();
 		exit(exit_val % 256);
 	}
-	for (i = 0; argv[i]; i++) free(argv[i]);
+
+	for (i = 0; argv[i]; i++)
+		free(argv[i]);
+
 	free(argv);
 	free(line);
+	_free_env();
 	exit(last_status);
 }
